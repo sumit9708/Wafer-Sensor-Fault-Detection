7604a51efeecda87420bf7cd4fc5b8a3a8d7f9f7
Sumit <sumitbhagat498@gmail.com>
2022-08-19 13:00:51 +0530

    Data Ingestion part completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    version="0.0.1",
+    description="ML project",
+    author="Sumit Bhagat", 
+    packages=find_packages(),
+    license="MIT"
+)
